d36733c1341989d8a16c096013d03f72fce95517
kusuma-pinninti <110592940+kusuma-pinninti@users.noreply.github.com>
2025-02-17 19:59:33 +0530

    indentation

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def text_preprocess(text):
 
 # Function to scrape product info from Amazon
 def scrape_amazon_product_info(url):
-  headers = {
+     headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                   '(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9',
